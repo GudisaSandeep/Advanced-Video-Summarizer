b0bbeba4006edbe14f231aa28872690c5ac11c51
Sandeep Gudisa <137704913+GudisaSandeep@users.noreply.github.com>
2024-10-26 14:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(name)
 # Load environment variables
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-#GEMINI_API_KEY="AIzaSyAMYpivJ4uwYaQdbPoRCPy-qXqAf0G8w1A"
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel(model_name="gemini-1.5-pro")
 def extract_video_id(url):
